86b970c1959bbd6fed9dc34630f7ce985e624c11
Colin Oraskovich <Colin.Oraskovich@gmail.com>
2019-02-15 11:01:35 -0600

    Initializing the first round of the Wx_App development to GitHub.

---
+++ User/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class AuthUser(models.Model):
+    id = models.AutoField(db_colummn = 'ID', primary_key = True)
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
